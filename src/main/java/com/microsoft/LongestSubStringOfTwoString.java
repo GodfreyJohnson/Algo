3aca5e7f8b2e5296cc7c685b7ab0ac53361ea689
jiangge_sx <jiangge_sx@qiyi.com>
2016-09-12 19:36:16 +0800

    catalan number

---
+++ src/main/java/com/microsoft/LongestSubStringOfTwoString.java
@@ -0,0 +1,20 @@
+package com.microsoft;
+
+public class LongestSubStringOfTwoString {
+
+  public String solution(String s1, String s2) {
+    String result = null;
+//    if(s1 == null || s1.isEmpty() || s2 == null || s2.isEmpty()){
+//      return null;
+//    }
+
+    int start = 0;
+    int count = 0;
+    int i = 0, j = 0;
+    while(i < s1.length()) {
+
+    }
+
+    return result;
+  }
+}
